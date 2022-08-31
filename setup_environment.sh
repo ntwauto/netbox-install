cc44667a93ab93a3ce2f22d42076c12e9f0c14f5
ntwauto <hammani.b@gmail.com>
2022-08-31 11:45:48 -0400

    Create setup_environment.sh

---
+++ setup_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# run this file within the directory to create your virtual environment properly
+#
+$ ./setup_environment.sh
+echo "Cloning the netbox-install repo"
+git clone https://github.com/ntwauto/netbox-install.git
+cd netbox-install/
+echo "Creating a local virtual environment venv"
+python3 -m venv venv
+echo "Sourcing the virtual environment"
+source venv/bin/activate
+echo "Updating pip wheel and setuptools"
+pip install -U pip wheel setuptools
+echo "Installing required python packages"
+pip install -r requirements.txt
